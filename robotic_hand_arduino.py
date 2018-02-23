bed4d83d5772d0a2c7b7df68baabdc8217cd9b4c
Roman Fasakhov <fasashen@gmail.com>
2018-02-23 21:35:02 +0300

    Initial commit

---
+++ robotic_hand_arduino.py
@@ -0,0 +1,90 @@
+import serial
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+import time
+import struct
+
+def establish_connection(serialport='/dev/cu.wchusbserial1420',baudrate=115200):
+    try:
+        arduino = serial.Serial(serialport, baudrate)
+    except:
+        print('Could not find serial port')
+
+    time_elapsed = 0
+    timeout = 5
+    connection_established = False
+    first_byte = None
+
+    t = time.time()
+    while time_elapsed < timeout:
+
+        time_elapsed = time.time() - t
+
+        if arduino.inWaiting():
+            first_byte = arduino.read()
+
+        if first_byte == b'A':
+            arduino.write(b'h')
+            connection_established = True
+            time_elapsed = timeout + 1
+
+    if connection_established:
+        print('Connection established in {} seconds.'.format(np.round(time.time() - t, 2)))
+        return arduino
+    else:
+        print('Connection failed.')
+        arduino.close()
+        return None
+
+def fread(fid, nelements, dtype):
+
+    """Equivalent to Matlab fread function"""
+
+    if dtype is np.str:
+        dt = np.uint8  # WARNING: assuming 8-bit ASCII for np.str!
+    else:
+        dt = dtype
+
+    data_array = np.fromfile(fid, dt, nelements)
+    data_array.shape = (nelements, 1)
+
+    return data_array
+
+def realtime_emg(ard):
+    if ard:
+        packsize = 17
+        numread = 20
+        k = 0
+        data = []
+
+        plt.ion()
+        plt.ylabel('Voltage')
+        plt.xlabel('Ts')
+        plt.axis([0, 256,-10,260])
+
+
+        while True:
+            if ard.inWaiting() >= numread * packsize:
+                for i in range(numread):
+                    # A = struct.unpack('{}B'.format(packsize),ard.read(packsize))
+                    A = ard.read(packsize)
+                    # A = np.array(br,dtype=np.uint8)
+                    print(A.decode('utf-8', 'ignore'))
+                    # data.append(abs(A[5]-A[4]))
+
+                    k = k + 1
+                    # plt.plot(range(len(data)), data)
+                    # plt.pause(0.05)
+
+                if k >= 1:
+                    k = 0
+                    break
+
+
+        ard.close()
+        print('Connection closed.')
+
+
+ard = establish_connection()
+realtime_emg(ard)
