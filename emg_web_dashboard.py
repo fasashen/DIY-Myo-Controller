f0751af0d13f63f6874920d2c4435d412b080e4e
Roman Fasakhov <fasashen@gmail.com>
2018-04-30 22:13:23 +0300

    Commit

--- emg_web_dashboard.py
+++ emg_web_dashboard.py
@@ -15,6 +15,15 @@ import requests
 
 
 app = dash.Dash(__name__)
+
+external_css = ["https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"]
+for css in external_css:
+    app.css.append_css({"external_url": css})
+
+external_js = ['https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js']
+for js in external_css:
+    app.scripts.append_script({'external_url': js})
+
 app.layout = html.Div([
 
         html.Div([
@@ -36,35 +45,10 @@ app.layout = html.Div([
 
         ], className='row', style={'margin-left':30,'margin-right':30,'vertical-align': 'middle'}),
 
-        html.Div([
-            html.Div(
-                children='Dash time: ',
-                className="col s1"),
-
-            html.Div(
-                children='Server time: ',
-                className="col s1")
-        ],
+        html.Div(
             id='time-div',
             className="row",
             style={'margin-left':30,'margin-right':30,'vertical-align': 'middle'}),
-
-        html.Div([
-            html.Div([
-                dcc.Graph(
-                    id='voltage-graph',
-                    animate=True
-                    )
-            ], className="col s6"),
-
-            html.Div([
-                dcc.Graph(
-                    id='freq-graph',
-                    animate=True
-                    )
-            ], className="col s6")
-
-        ], className="row", style={'width':'100%','float':'center'}),
 
         html.Div([
 
@@ -73,23 +57,45 @@ app.layout = html.Div([
                 animate=True
                 )
 
-        ], className="row", style={'width':'100%','float':'center', 'display': 'inline-block'}),
+            ], className="row", style={'width':'100%','float':'center', 'display': 'inline-block'}),
 
-        dcc.Interval(
-            id='graph-update',
-            interval=1*1000),
+            dcc.Interval(
+                id='graph-update',
+                interval=1*1000),
 
-        dcc.Interval(
-            id='nanstd-update',
-            interval=1*1000),
+            dcc.Interval(
+                id='nanstd-update',
+                interval=1*1000),
 
-        dcc.Interval(
-            id='data-update',
-            interval=1*2000),
+            dcc.Interval(
+                id='data-update',
+                interval=1*2000),
 
-        html.Div(id='hidden-div', style={'display':'none'})
+            html.Div(id='hidden-div', style={'display':'none'}),
 
-    ], className="container",style={'float':'center','width':'95%','margin-left':30,'margin-right':30,'max-width':2000}
+            html.Div([
+                html.Div([
+                    html.Div([
+                        dcc.Graph(
+                            id='voltage-graph',
+                            animate=True
+                            )
+                    ], className="col s6"),
+
+                    html.Div([
+                        dcc.Graph(
+                            id='freq-graph',
+                            animate=True
+                            )
+                    ], className="col s6")
+
+                ], className="row", style={'width':'100%','float':'center'}),
+                ], className='row', style={'margin-left':30,'margin-right':30,'vertical-align': 'middle'}),
+
+        ],
+        className="container",
+        style={'float':'center','width':'95%','margin-left':30,'margin-right':30,'max-width':2000}
+
 )
 
 
@@ -102,18 +108,17 @@ def update_all_data(channels_to_plot):
 
     r = requests.get('http://localhost:5000/emg/{}'.format(channels_to_plot))
     data = r.json()
+    print(r,data['server_time'])
 
 
-    dash_time = html.Div(
-        children='Dash time: ' + strftime("%H:%M:%S", gmtime()),
-        className="col s1")
 
-    server_time = html.Div(
-        children='Server time: ' + str(data['server_time']),
-        className="col s1")
+    server_time = html.H6(
+        children='Server time: ' + data['server_time'],
+        className="col s12",
+        style={'width':'100%','float':'center','text-align': 'center'})
 
 
-    return [dash_time, server_time]
+    return [server_time]
 
 
 
@@ -168,7 +173,7 @@ def update_nanstd_plot():
 
     return {'data': nanstd_plots,'layout' : go.Layout(xaxis=dict(range=[min(x_nanstd),max(x_nanstd)]),
                                                 yaxis=dict(range=[minval,maxval]),
-                                                title="Standard deviation")}
+                                                title="Standard deviation, Plot Time: {}".format(strftime("%H:%M:%S", gmtime())))}
 
 @app.callback(Output('freq-graph', 'figure'),
               events=[Event('graph-update', 'interval')])
@@ -195,13 +200,7 @@ def update_freq_plot():
                                                 yaxis=dict(range=[min(freq[0]),max(freq[0])]),
                                                 title="Fourie transform")}
 
-external_css = ["https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"]
-for css in external_css:
-    app.css.append_css({"external_url": css})
 
-external_js = ['https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js']
-for js in external_css:
-    app.scripts.append_script({'external_url': js})
 
 def max_min(lists):
     maxval = max(lists[0])
